fb0eae1373496761db12ff12e771e13458c72d6f
Hebx <lord.noctx@gmail.com>
2024-02-28 11:17:53 +0100

    Rust Practices Done

--- rust/homeworks/homework4/primitive_types/primitive_types4.rs
+++ rust/homeworks/homework4/primitive_types/primitive_types4.rs
@@ -2,13 +2,12 @@
 // Get a slice out of Array a where the ??? is so that the test passes.
 // Execute `zustlings hint primitive_types4` for hints!!
 
-// I AM NOT DONE
-
 #[test]
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
+    let nice_slice = &a[1..4];
+    // Slices in rust are inclusive on the start (i here == 1) and exclusive on the end (i - 1)
 
     assert_eq!([2, 3, 4], nice_slice)
 }
